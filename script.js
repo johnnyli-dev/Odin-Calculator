8575c8aa4d24dbd42f526310ecb7bd96dcc2c7e6
Johnny Li <jql1227@gmail.com>
2022-12-05 00:33:07 -0800

    Add single operation computing
    Add the ability to computer single operations such as 1+1 or 12*4. No
    complex equations with multiple operators yet.

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
 let displayValue = [];
+let displayTemp = [];
+let displayCalc = "";
 const buttons = document.querySelectorAll(".numbers");
 const lButtons = document.querySelectorAll(".largerButtons");
 const displayPort = document.querySelector(".CalcDisplay");
 
 function add(a,b) {
-    return a + b;
+    return (parseInt(a) + parseInt(b)).toString();
 }
 
 function subtract(a,b) {
-    return a - b;
+    return (a - b).toString();
 }
 
 function multiply(a,b) {
-    return a * b;
+    return (a * b).toString();
 }
 
 function divide(a,b) {
-    return a / b;
-}
-
-function operate(operator, a,b) {
-    if(operator == "+") {
-        return add(a,b);
-    } else if(operator === "-") {
-        return subtract(a,b);
-    } else if(operator === "*") {
-        return multiply(a,b);
-    } else {
-        return divide(a,b);
-    }
+    return (a / b).toString();
 }
 
 buttons.forEach(element => {
     element.addEventListener("click", function() {
-        if(element.id === "C") {
+    if (element.id === "=") {
+            displayValue.push(displayCalc);
+            evaluate();
+            displayTemp = displayValue;
             update();
-        } else if(element.id === "D") {
+            console.log(displayValue);
+            displayTemp = [];
+            displayCalc = "";
+            displayValue = [];
+    } else {
+        if(element.id === "+") {
+            displayValue.push(displayCalc, element.id);
+            displayTemp.push(element.id);
+            console.log(displayValue);
             update();
-        } else if (element.id ==="=") {
+            displayCalc = "";
+        } else if(element.id === "-") {
+            displayValue.push(displayCalc, element.id);
+            displayTemp.push(element.id);
+            console.log(displayValue);
             update();
+            displayCalc = "";
+        } else if(element.id === "*") {
+            displayValue.push(displayCalc, element.id);
+            displayTemp.push(element.id);
+            console.log(displayValue);
+            update();
+            displayCalc = "";
+        } else if (element.id === "/") {
+            displayValue.push(displayCalc, element.id);
+            displayTemp.push(element.id);
+            console.log(displayValue);
+            update();
+            displayCalc = "";
         } else {
-            displayValue.push(element.id);
+            displayCalc += element.id;
+            displayTemp.push(element.id);
             update();
+        }
         }
     });
 });
@@ -51,28 +71,58 @@ lButtons.forEach(element => {
         if(element.id === "C") {
             //clear
             displayPort.innerHTML = "0";
+            displayCalc = "";
+            displayTemp = [];
             displayValue = [];
-        } else if(element.id === "D") {
-            //delete
-            displayValue.splice(displayValue.length-1)
-            update();
         } else {
-            //equals
-            evaluate();
+            //delete
+            displayCalc = (displayCalc.slice(0, displayCalc.length - 1));
+            console.log(displayCalc);
+            displayTemp.splice(displayTemp.length-1);
             update();
         }
     });
 });
 
 function update() {
-    if(displayValue.length < 1) {
+    if(displayTemp.length < 1) {
         displayPort.innerHTML = "0";
-        displayValue = [];
+        displayTemp = [];
     } else {
-        displayPort.innerHTML = displayValue.join("");
+        displayPort.innerHTML = displayTemp.join("");
     }
 }
 
 function evaluate() {
-    
+    for(let i = 0; i < displayValue.length; i++) {
+        if(displayValue[i] === "*" || displayValue[i] === "/") {
+            if(displayValue[i] === "*") {
+                let a = displayValue[i-1];
+                let b = displayValue[i + 1];
+                displayValue[i] = multiply(a,b);
+                displayValue.splice(i+1,1);             
+                displayValue.splice(i-1,1);
+            } else {
+                let a = displayValue[i-1];
+                let b = displayValue[i + 1];
+                displayValue[i] = divide(a,b);
+                displayValue.splice(i+1,1);             
+                displayValue.splice(i-1,1);
+            }
+        } else if (displayValue[i] === "+" || displayValue[i] === "-"){
+            if(displayValue[i] === "+") {
+                let a = displayValue[i-1];
+                let b = displayValue[i + 1];
+                displayValue[i] = add(a,b);
+                displayValue.splice(i+1,1);             
+                displayValue.splice(i-1,1);
+            } else {
+                let a = displayValue[i-1];
+                let b = displayValue[i + 1];
+                displayValue[i] = subtract(a,b);
+                displayValue.splice(i+1,1);             
+                displayValue.splice(i-1,1);
+            }
+        }
+    }
 }
